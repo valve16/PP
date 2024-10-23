6ec80f09f94220fbb099fd442688282c2b90d6a8
valve16 <glushkovoleg2004@yandex.ru>
2024-10-23 22:58:12 +0300

    add setPriority

--- lw3/lw3/lw3.cpp
+++ lw3/lw3/lw3.cpp
@@ -12,7 +12,7 @@ DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
     int threadNum = *static_cast<int*>(lpParam);  // Получение номера потока
     const int operationCount = 15;
 
-    std::string filename = (threadNum == 1) ? "output3.txt" : "output4.txt";
+    std::string filename = (threadNum == 1) ? "output5.txt" : "output6.txt";
     for (int i = 0; i < operationCount; i++)
     {
         for (int j = 0; j < 10000; ++j)
@@ -39,7 +39,7 @@ int main(int argc, char* argv[])
     const int N = 2;          
     int threadNums[N];
     HANDLE handles[N];
-    //std::cin.get();
+    std::cin.get();
 
     startTime = timeGetTime();
 
@@ -48,6 +48,14 @@ int main(int argc, char* argv[])
     {
         threadNums[i] = i + 1;
         handles[i] = CreateThread(NULL, 0, &ThreadProc, &threadNums[i], CREATE_SUSPENDED, NULL);
+        if (i == 0)
+        { 
+            SetThreadPriority(handles[i], THREAD_PRIORITY_HIGHEST); // Низкий приоритет для первого потока +2 для первого потока
+        }
+        //else
+        //{
+        //    SetThreadPriority(handles[i], THREAD_PRIORITY_TIME_CRITICAL); // Высокий приоритет для второго потока +15
+        //}
     }
 
     for (int i = 0; i < N; ++i)
