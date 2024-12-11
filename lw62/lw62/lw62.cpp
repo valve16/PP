0d7734753b1b8837dd656411ad57a96d70b7342d
valve16 <glushkovoleg2004@yandex.ru>
2024-12-11 22:44:35 +0300

    add lw62

---
+++ lw62/lw62/lw62.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <omp.h>
+
+int main()
+{
+    int x = 44;
+#pragma omp parallel for lastprivate(x)
+    for (int i = 0; i <= 10; i++) 
+    {
+        x = i;
+        printf("Thread number: %d x: %d\n", omp_get_thread_num(), x);
+    }
+    printf("x is %d\n", x);
+}
